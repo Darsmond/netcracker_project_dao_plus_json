fe01917be72bd5f52eb85f406fb380404226b786
Ars <dantenonero@gmail.com>
2018-01-24 18:02:16 +0300

    finally

---
+++ src/main/java/DaoFactory.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/** Фабрика объектов для работы с базой данных */
+public interface DaoFactory {
+
+    /** Возвращает подключение к базе данных */
+    public Connection getConnection() throws SQLException;
+
+    /** Возвращает объект для управления персистентным состоянием объекта Group */
+    public GroupDao getGroupDao(Connection connection);
+
+}
+
